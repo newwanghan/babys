70e1fd42b16f255fef114a73da46f64a00b58bf5
wanghan <newwanghan@163.com>
2022-01-02 17:11:48 +0800

    test20220102-04

--- test01/models.py
+++ test01/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+
+class PersonInfo(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.IntegerField()
+    hireDate = models.DateField()
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = '人员信息'
+        verbose_name_plural = verbose_name
+
+
+class Vocation(models.Model):
+    job = models.CharField(max_length=20)
+    title = models.CharField(max_length=20)
+    payment = models.IntegerField(null=True, blank=True)
+    name = models.ForeignKey(PersonInfo, on_delete=models.CASCADE, related_name='ps')
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        verbose_name = '职业信息'
+        verbose_name_plural = verbose_name
